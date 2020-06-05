ba58e7ff1ca965c98bfa96aa738a97ac9e316b7f
nekit-0123 <mekit-0123@mail.ru>
2020-06-05 16:20:14 +0300

    just correct

--- CheckDir.cpp
+++ CheckDir.cpp
@@ -1,11 +1,17 @@
 ﻿#include <windows.h>
+#include <string.h>
 
 // Добавить в автозагрузку
 LONG InsertAutoRun()
 {
-	HKEY hKey;
-	char szPath[256];
-	GetModuleFileNameA(NULL, szPath, sizeof(szPath));
+	HKEY hKey = NULL;
+	char szPath[256] = { NULL };
+	if (GetModuleFileNameA(NULL, szPath, sizeof(szPath)) <= 0)
+	{
+		std::cout<<"Error GetModuleFileName";
+		return -1;
+	}
+
 	RegCreateKeyEx(HKEY_LOCAL_MACHINE,
 		"Software\\Microsoft\\Windows\\CurrentVersion\\Run",
 		NULL,
@@ -20,6 +26,11 @@ LONG InsertAutoRun()
 	{
 		RegSetValueEx(hKey, "Check Dir", NULL, REG_SZ, (LPBYTE)szPath, (DWORD)strlen(szPath));
 		RegCloseKey(hKey);
+	}
+	else 
+	{
+		std::cout<<"Error RegCreateKeyEx";
+		return -2;
 	}
 
 	return 0;
@@ -51,6 +62,12 @@ void CheckTempRath(const char* WinTemp, size_t size)
 		FILE_FLAG_BACKUP_SEMANTICS,
 		NULL);
 
+	if (hDir == INVALID_HANDLE_VALUE)
+	{
+		std::cout<<"Error CreateFile";
+		return;
+	}
+
 	char *CheckFile = new char[size + sizeof("stopme.txt")+1];
 	ZeroMemory(CheckFile, size + sizeof("stopme.txt"));
 	strcat_s(CheckFile, size, WinTemp);
@@ -60,18 +77,25 @@ void CheckTempRath(const char* WinTemp, size_t size)
 	{
 		if (isFileExist(CheckFile))
 		{
-			DeleteAutoRun();
+			if (DeleteAutoRun() != 0)
+				std::cout<<"Error RegDeleteKey";
 			break;
 		}
 
 		DWORD dwBytesReturned = 0;
 		if (ReadDirectoryChangesW(hDir, (LPVOID)&strFileNotifyInfo, sizeof(strFileNotifyInfo), FALSE, FILE_NOTIFY_CHANGE_LAST_WRITE, &dwBytesReturned, NULL, NULL) == 0)
+		{
+			std::cout<<"Error ReadDirectoryChangesW";
 			break;
+		}
 		else
 		{
 			const char* Message = (char *)strFileNotifyInfo.FileName;	
 			if (ReportEventA(event_log, EVENTLOG_SUCCESS, 0, 0, NULL, 1, 0, &Message, NULL) == 0)
+			{
+				std::cout<<"Error ReportEventA";
 				break;
+			}
 		}
 
 		Sleep(20);
@@ -79,22 +103,29 @@ void CheckTempRath(const char* WinTemp, size_t size)
 
 	delete[] CheckFile;
 	CheckFile = nullptr;
-
 }
 
 int main()
 {
 	const char * commandStr = GetCommandLineA();
-
 	char WinTempStr[MAX_PATH + 1];
-	GetTempPath(MAX_PATH + 1, WinTempStr); // Получение директории папки temp
+	if (GetTempPath(MAX_PATH + 1, WinTempStr) == 0)// Получение директории папки temp
+	{
+		std::cout<<"Error ReadDirectoryChangesW";
+		return -1;
+	}
+
 	size_t lsize = strlen(WinTempStr) + 1;
-	
 	char OurDir[MAX_PATH + 1];
-	GetCurrentDirectory(MAX_PATH + 1, OurDir);	// Получение директории откуда запущены
+	if (GetCurrentDirectory(MAX_PATH + 1, OurDir) == 0)	// Получение директории откуда запущены
+	{
+		std::cout<<"Error GetCurrentDirectory";
+		return -1;
+	}
 
 	// Если мы не в папке темп
-	if (WinTempStr != OurDir)
+	
+	if (strcmp(WinTempStr, OurDir) != 0)
 	{
 		char *strPath = new char[lsize + sizeof("CheckDir.exe")];
 		ZeroMemory(strPath, lsize);
@@ -103,18 +134,21 @@ int main()
 
 		if (CopyFile(commandStr, strPath, 1) > 0)
 			ShellExecute(NULL, "open", commandStr, strPath, NULL, SW_RESTORE);
+		else 
+			std::cout<<"Error CopyFile";
 
 		delete[] strPath;
 		strPath = nullptr;
 
 		return 0;
 	}
-	else if (WinTempStr == OurDir)
+	else if (strcmp(WinTempStr, OurDir) == 0)
 	{
 		if (isFileExist(commandStr)) 
 			DeleteFile(commandStr);
 
-			InsertAutoRun();
+			if (InsertAutoRun() != 0)
+			 return -1; 
 	}
 
 	CheckTempRath(WinTempStr, lsize);
